20ff16eaa5633778257dd7d26eda0759edcd0026
Mackenzie Ortiz <mlubben1@gmail.com>
2024-03-24 21:04:39 -0400

    Add files via upload

---
+++ profile-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent
+  ],
+  imports: [
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
