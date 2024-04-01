ea01f0ac2279c564935aa7dc2216d7a6336134d8
Mackenzie Ortiz <mlubben1@gmail.com>
2024-03-31 21:59:59 -0400

    Add files via upload

---
+++ composer-app/src/app/composer-list/composer-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+export default class Composer {
+  fullName: string;
+  genre: string;
+
+  constructor(fullName: string, genre: string) {
+    this.fullName = fullName;
+    this.genre = genre;
+  }
+}
+
+@Component({
+  selector: 'app-composer-list',
+  templateUrl: './composer-list.component.html',
+  styleUrls: ['./composer-list.component.css']
+})
+export class ComposerListComponent implements OnInit {
+
+  composers: Array<Composer>;
+
+  constructor() {
+    this.composers = [
+      new Composer("Ludwig van Beethoven", "Classical"),
+      new Composer("Johann Sebastian Bach", "Classical"),
+      new Composer("Wolfgang Amadeus Mozart", "Classical"),
+      new Composer("Johannes Brahms", "Classical"),
+      new Composer("Joseph Haydn", "Classical")
+    ]
+   }
+
+  ngOnInit(): void {
+  }
+
+}
