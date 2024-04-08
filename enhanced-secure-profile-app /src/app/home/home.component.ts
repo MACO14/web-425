e696f84b5247a6d5f5e3720b3d03c3113fb37b96
Mackenzie Ortiz <mlubben1@gmail.com>
2024-04-07 23:27:04 -0400

    Add files via upload

---
+++ enhanced-secure-profile-app /src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+/**
+ * Title: home.component.ts
+ * Author: Mackenzie Lubben-Ortiz
+ * Date: 07 April 2024
+ * Description: Home component
+ */
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
