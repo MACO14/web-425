01cff1cfa02333a188f070b72d1d65554564da44
Mackenzie Ortiz <mlubben1@gmail.com>
2024-04-07 21:55:16 -0400

    Add files via upload

---
+++ week-3/enhanced-composer-app/src/app/composer-list/composer-list.component.ts
@@ -0,0 +1,29 @@
+/**
+ * Title: composer-list.component.ts
+ * Author: Mackenzie Lubben-Ortiz
+ * Date: 07 April 2024
+ * Description: Composer list component; displaying a list of composers
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+import { IComposer } from '../composer.interface';
+import { Composer } from '../composer.class';
+
+@Component({
+  selector: 'app-composer-list',
+  templateUrl: './composer-list.component.html',
+  styleUrls: ['./composer-list.component.css']
+})
+export class ComposerListComponent implements OnInit {
+
+  composers: Array<IComposer>;
+
+  constructor() {
+    this.composers = new Composer().getComposers();
+
+  }
+  ngOnInit(): void {
+  }
+
+}
