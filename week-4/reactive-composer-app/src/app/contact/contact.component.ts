45cdf5e921cc06bd16735295ce6d27c575541cc4
Mackenzie Ortiz <mlubben1@gmail.com>
2024-04-10 20:44:21 -0400

    Add files via upload

---
+++ week-4/reactive-composer-app/src/app/contact/contact.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Title: contact.component.ts
+ * Author: Mackenzie Lubben-Ortiz
+ * Date: 07 April 2024
+ * Description: contact component
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
