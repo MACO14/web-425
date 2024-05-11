98ea2e5a35e9ed4b3d921620fb789d3d877add60
Mackenzie Ortiz <mlubben1@gmail.com>
2024-05-10 21:51:36 -0400

    Add files via upload

---
+++ week-8/in-n-out-books/src/app/wishlist-create/wishlist-create.component.ts
@@ -0,0 +1,39 @@
+/**
+ * Title: wishlist-create.component.ts
+ * Author: Mackenzie Lubben-Ortiz
+ * Date: 23 April 2024
+ * Description: wishlist-create component
+ */
+
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { IWishlistItem } from '../wishlist-item.interface';
+
+@Component({
+  selector: 'app-wishlist-create',
+  templateUrl: './wishlist-create.component.html',
+  styleUrl: './wishlist-create.component.scss'
+})
+export class WishlistCreateComponent implements OnInit {
+
+  @Output() addItemEmitter = new EventEmitter<IWishlistItem>();
+
+  item: IWishlistItem;
+
+  constructor() {
+    this.item = {} as IWishlistItem;
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  addItem() {
+    this.addItemEmitter.emit({
+      title: this.item.title,
+      authors: this.item.authors
+    })
+
+    this.item = {} as IWishlistItem;
+  }
+
+}
---
+++ week-8/in-n-out-books/src/app/wishlist-item.interface.ts
@@ -0,0 +1,10 @@
+/**
+ * Title: wishlist-item.interface.ts
+ * Author: Mackenzie Lubben-Ortiz
+ * Date: 23 April 2024
+ * Description: wishlist item interface
+ */
+export interface IWishlistItem {
+  title: string;
+  authors: string;
+}
